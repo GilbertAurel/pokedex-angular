d5b2d8eb5a8e797409ab01fb6192ef7cdee1aa1a
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-10-11 16:40:41 +0700

    feat: add get pokemon http client request

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Pokemon } from 'src/app/interfaces/pokemon';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonService {
-  constructor() {}
+  limit: number = 20;
+  offset: number = 0;
+  private pokeAPIUrl = `https://pokeapi.co/api/v2/pokemon?limit=${this.limit}offset=${this.offset}`;
+
+  constructor(private http: HttpClient) {}
+
+  getAllPokemon(): Observable<Pokemon[]> {
+    return this.http
+      .get<Pokemon[]>(this.pokeAPIUrl)
+      .pipe(map((res: any) => res.results));
+  }
 }
