d68d0d20a7b3dea05502fd292033fe005053efd8
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-10-13 09:19:53 +0700

    feat: add on init to dashboard to get all pokemon data

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/interfaces/pokemon';
+
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
-  constructor() {}
+export class DashboardComponent implements OnInit {
+  pokemonList: Pokemon[] = [];
+
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit() {
+    this.pokemonService
+      .getPokemonDetails('initial')
+      .subscribe((res) => (this.pokemonList = res));
+  }
 }
