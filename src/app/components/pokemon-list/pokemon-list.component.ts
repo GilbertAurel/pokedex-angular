6504e278fd1488a243f9e9f6a312cabd06eb8bc0
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-10-13 12:32:41 +0700

    feat: add show pokemon image and name functions

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from 'src/app/interfaces/pokemon';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-list.component.scss'],
 })
 export class PokemonListComponent {
+  @Input() pokemonList!: Pokemon[];
+
   constructor() {}
 }
