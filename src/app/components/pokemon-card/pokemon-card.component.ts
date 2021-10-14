72ddcfbc6a834d3d568bcdc4fcfe4cce4dc9de49
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-10-14 12:41:07 +0700

    feat: add emmitable click function for pokemon card

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Pokemon } from 'src/app/interfaces/pokemon';
 
 @Component({
@@ -8,6 +8,11 @@ import { Pokemon } from 'src/app/interfaces/pokemon';
 })
 export class PokemonCardComponent {
   @Input() pokemon!: Pokemon;
+  @Output() selectPokemon = new EventEmitter();
 
   constructor() {}
+
+  onClick() {
+    this.selectPokemon.emit(this.pokemon);
+  }
 }
