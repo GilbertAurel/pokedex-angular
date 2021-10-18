4fc36220e1e119a2f0da455bc728279c08ba72c7
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-10-18 11:16:44 +0700

    feat: add list service with prev and next button throttle function

---
+++ src/app/services/list.service.ts
@@ -0,0 +1,28 @@
+import { ElementRef, Injectable } from '@angular/core';
+import { fromEvent, interval } from 'rxjs';
+import { throttle } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ListService {
+  constructor() {}
+
+  prevBtnClicked(btn: ElementRef, list: ElementRef) {
+    return fromEvent(btn.nativeElement, 'click')
+      .pipe(throttle(() => interval(500)))
+      .subscribe(
+        () =>
+          (list.nativeElement.scrollLeft = list.nativeElement.scrollLeft - 300)
+      );
+  }
+
+  nextBtnClicked(btn: ElementRef, list: ElementRef) {
+    return fromEvent(btn.nativeElement, 'click')
+      .pipe(throttle(() => interval(500)))
+      .subscribe(
+        () =>
+          (list.nativeElement.scrollLeft = list.nativeElement.scrollLeft + 300)
+      );
+  }
+}
