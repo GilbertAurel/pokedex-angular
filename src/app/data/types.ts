6dbd28a05940fe2704c33f7ab12f4e922a062e62
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-10-18 15:02:17 +0700

    feat: add icons for pokemon type

---
+++ src/app/data/types.ts
@@ -0,0 +1,81 @@
+interface Type {
+  name: string;
+  iconUrl: string;
+}
+
+const baseUrl = 'assets/icons/Icon_';
+
+export const TYPES: Type[] = [
+  {
+    name: 'bug',
+    iconUrl: `${baseUrl}Bug.png`,
+  },
+  {
+    name: 'dark',
+    iconUrl: `${baseUrl}Dark.png`,
+  },
+  {
+    name: 'dragon',
+    iconUrl: `${baseUrl}Dragon.png`,
+  },
+  {
+    name: 'electric',
+    iconUrl: `${baseUrl}Electric.png`,
+  },
+  {
+    name: 'fairy',
+    iconUrl: `${baseUrl}Fairy.png`,
+  },
+  {
+    name: 'fighting',
+    iconUrl: `${baseUrl}Fighting.png`,
+  },
+  {
+    name: 'fire',
+    iconUrl: `${baseUrl}Fire.png`,
+  },
+  {
+    name: 'flying',
+    iconUrl: `${baseUrl}Flying.png`,
+  },
+  {
+    name: 'ghost',
+    iconUrl: `${baseUrl}Ghost.png`,
+  },
+  {
+    name: 'grass',
+    iconUrl: `${baseUrl}Grass.png`,
+  },
+  {
+    name: 'ground',
+    iconUrl: `${baseUrl}Ground.png`,
+  },
+  {
+    name: 'ice',
+    iconUrl: `${baseUrl}Ice.png`,
+  },
+  {
+    name: 'normal',
+    iconUrl: `${baseUrl}Normal.png`,
+  },
+  {
+    name: 'poison',
+    iconUrl: `${baseUrl}Poison.png`,
+  },
+  {
+    name: 'psychic',
+    iconUrl: `${baseUrl}Psychic.png`,
+  },
+  {
+    name: 'rock',
+    iconUrl: `${baseUrl}Rock.png`,
+  },
+  {
+    name: 'steel',
+    iconUrl: `${baseUrl}Steel.png`,
+  },
+  {
+    name: 'water',
+    iconUrl: `${baseUrl}Water.png`,
+  },
+];
